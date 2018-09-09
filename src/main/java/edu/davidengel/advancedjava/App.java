38d12682ff24bcaf0a5cc4c22513909c65ce2b31
dengel223 <43100041+dengel223@users.noreply.github.com>
2018-09-09 13:08:04 -0400

    Revert "Assignment 1 for Dave Engel Completed"

--- src/main/java/edu/davidengel/advancedjava/App.java
+++ src/main/java/edu/davidengel/advancedjava/App.java
@@ -1,8 +1,5 @@
 package edu.davidengel.advancedjava;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Hello world!
  *
@@ -11,28 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        //setup data
-        Cpu cpu = new Cpu("Intel", 200.0d, 4.1);
-        Motherboard motherboard = new Motherboard("Intel",300.0d);
-        PowerSupply powerSupply = new PowerSupply("Corsair",650,400.0d);
-
-        List<ComputerComponent> components = new ArrayList();
-        components.add(cpu);
-        components.add(motherboard);
-        components.add(powerSupply);
-
-        Computer computer = new Computer( components, 1400, 100, 150000.0d);
-
-        System.out.println( "Created Computer Has the following info:" );
-
-        System.out.println("Target Revenue: " + computer.getTargetRevenue());
-
-        System.out.println("Projected Revenue: " + computer.calculateProjectedRevenue());
-
-        System.out.println("Projected Profit: " + computer.calculateProfit());
-
-        System.out.println("Total Cost: " + computer.calculateTotalCost());
-
+        System.out.println( "Hello World!" );
     }
 }
